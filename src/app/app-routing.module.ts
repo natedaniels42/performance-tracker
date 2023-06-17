8300f4df25ff5acd49f67caa8bdad2955825cb09
natedaniels42 <nathandanielsbassoon@gmail.com>
2023-06-17 17:45:05 -0500

    Updated deployment settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './components/page/page.component';
 
 const routes: Routes = [
-  {path: '/', component: PageComponent}
+  {path: '**', component: PageComponent}
 ];
 
 @NgModule({
